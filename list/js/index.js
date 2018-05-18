859b4f60c379a680b645032b2b473365eaa879da
Gál Péter <pepyta118@gmail.com>
2018-05-18 23:12:33 +0200

    Bought all function is now enabled

--- list/js/index.js
+++ list/js/index.js
@@ -7,16 +7,11 @@ function trashList(listid) {
         elem.parentNode.removeChild(elem);
     }, 300);
 
-    if (document.getElementById("listContainer").childElementCount == 0) {
-        document.getElementById('noList').style = '';
-    }
 }
 
 function boughtItem(itemid, quantity, listid) {
-    //document.getElementById('reqresponse').innerHTML = $.get("req.php?req=boughtItem;" + itemid + ";" + quantity + ";" + listid);
-
     var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    xhr.open('get', "req.php?req=boughtItem;" + itemid + ";" + quantity + ";" + listid, true);
+    xhr.open('get', "req.php?req=boughtItem;" + itemid + ";'" + quantity + "';" + listid, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             if (xhr.responseText == 0) {
@@ -25,15 +20,32 @@ function boughtItem(itemid, quantity, listid) {
                 elem.classList.add("scale-out");
                 setTimeout(function () {
                     elem.parentNode.removeChild(elem);
+
+
                 }, 300);
+                setTimeout(function () {
+                    var itemList = document.getElementById("itemList" + listid);
+                    if (itemList.childElementCount == 0) {
+                        itemList.classList.add('hide');
+                        console.log(itemList.classList);
+                    }
+                }, 310);
             } else {
                 document.getElementById("itemToDisplay" + itemid).innerHTML = xhr.responseText;
             }
         }
     }
+
     xhr.send();
-    var itemList = document.getElementById("itemList" + listid);
-    if (itemList.childElementCount == 0) {
-        itemList.classList.add = 'hidden';
+}
+
+function createList(name) {
+    var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('get', "req.php?req=createList;" + name, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            xhr.responseText
+        }
     }
+    xhr.send();
 }
