57273722a12b13e618aebbd062860a2d82522a61
Gál Péter <pepyta118@gmail.com>
2018-05-18 18:37:11 +0200

    Added landing page

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<HTML lang="en">
+
+<HEAD>
+    <title>Shopping List</title>
+
+    <meta name="author" content="Gál Péter">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="description" content="Shopping List is a simple online service where you can store your shopping list.">
+    <meta name="keywords" content="list, shopping, responsive, mobile-friendly">
+    <meta name="theme-color" content="#303f9f">
+    <meta name="google-signin-client_id" content="347334941281-4b5vmojlbpvkbth50rakji7cnadmonls.apps.googleusercontent.com">
+
+    <link rel="shortcut icon" href="list/img/web_hi_res_512.png">
+
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.1/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
+</HEAD>
+
+<BODY>
+    <div class="parallax-container">
+        <div class="parallax indigo">
+            <div class="top white-text center-align">
+                <h1>shoplist.ml</h1>
+                <h6>is an innovative solution to the shopping list</h6><br>
+                <a href="list/index.php">
+                    <div class="btn waves-effect waves-light indigo darken-2 ">Let's start shopping!</div>
+                </a>
+
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.parallax').parallax();
+        });
+
+    </script>
+</BODY>
+
+</HTML>
