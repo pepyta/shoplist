33f870de864f9c07aa3abcceb33e64a328a68688
Gál Péter <pepyta118@gmail.com>
2018-10-26 21:07:57 +0200

    Remove settings.php

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 
 //MYSQLI connection
-$conn = new mysqli("mysql.nethely.hu", "root", "", "list");
+$conn = new mysqli("localhost", "list", "17154ACBA0ED3B3C472355E452DA5EB0C084C1CB676E3319A2D668197B57986B", "list");
 
 //Make sure website works properly
 if ($conn->connect_error) {
@@ -14,8 +14,8 @@ if ($conn->connect_error) {
 $lang = 'en';
 
 //Recaptcha code (sitekey)
-$recaptcha = '';
+$recaptcha = '6LeRHF0UAAAAAA6hI6xnAYuKpE_8-LTwCTtzLPt4';
 
 //SALT for more security
-$salt = '';
+$salt = '7B43746159BBAAB36D3A3F5D475A231190596FB7795F0FE8AB71A9F424CB360F';
 ?>
